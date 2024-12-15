f2f7e045307e05afba35e7a0a06860c65b1866c5
admin <admin@DESKTOP-BUL8MPT.mshome.net>
2024-12-15 12:53:14 +0530

    Added my test case

---
+++ ORANGEHRM/src/test/java/com/OrangeHrm/RunnerScripts/AdminStory/Kousalya.java
@@ -0,0 +1,12 @@
+package com.OrangeHrm.RunnerScripts.AdminStory;
+
+import org.testng.annotations.Test;
+
+public class Kousalya
+{
+	@Test
+	public static void sampleTest()
+	{
+		System.out.println("I'am Kousalya");
+	}
+}
