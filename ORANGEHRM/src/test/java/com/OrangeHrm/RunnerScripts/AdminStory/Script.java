f56a65fac5cfe0f1542171851db04e94c23bd7f1
SLC <SLC@DESKTOP-H7K8J40.mshome.net>
2024-12-15 13:16:45 +0530

    commit msg

---
+++ ORANGEHRM/src/test/java/com/OrangeHrm/RunnerScripts/AdminStory/Script.java
@@ -0,0 +1,13 @@
+
+package com.OrangeHrm.RunnerScripts.AdminStory;
+
+import org.testng.annotations.Test;
+
+public class Script {
+	@Test
+	public void Scriptbasic()
+	{
+System.out.println("hi");
+	}
+
+}
