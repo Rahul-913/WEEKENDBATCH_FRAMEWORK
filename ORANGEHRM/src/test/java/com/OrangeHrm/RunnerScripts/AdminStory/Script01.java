c146f04515a6a87643b6cccd18e817a32dd111a7
dhira <dhira@DHIRAJ>
2024-12-16 16:16:36 +0530

    Dhiraj

---
+++ ORANGEHRM/src/test/java/com/OrangeHrm/RunnerScripts/AdminStory/Script01.java
@@ -0,0 +1,13 @@
+package com.OrangeHrm.RunnerScripts.AdminStory;
+
+import org.testng.annotations.Test;
+
+public class Script01 
+{
+	@Test
+	public void get()
+	{
+		System.out.println("Success");
+	}
+
+}
